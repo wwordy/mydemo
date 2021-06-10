e85b5082c3c2d87be4fbf4145eb2b12632de7fac
liuwangwang <liuwangwang@corp.netease.com>
2021-06-10 23:07:32 +0800

    1111

---
+++ src/main/java/com/netease/wwordy/GitTest.java
@@ -0,0 +1,11 @@
+package com.netease.wwordy;
+
+/**
+ * @author liuwangwang@corp.netease.com
+ * @date 2021/6/10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
