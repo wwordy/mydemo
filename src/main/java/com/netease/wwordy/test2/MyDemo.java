1e1a61c96faba793a1032825f076ccd344cc0ad4
liuwangwang <liuwangwang@corp.netease.com>
2021-06-17 10:32:20 +0800

    master提交

---
+++ src/main/java/com/netease/wwordy/test2/MyDemo.java
@@ -0,0 +1,11 @@
+package com.netease.wwordy.test2;
+
+/**
+ * @author liuwangwang@corp.netease.com
+ * @date 2021/6/17
+ */
+public class MyDemo {
+    public static void main(String[] args) {
+        System.out.println("myDemo.......");
+    }
+}
